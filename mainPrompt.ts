6a3880fe72173961890f0966a41d4bdf436f03f1
Héctor Realpe <hrealpe@gmail.com>
2021-04-22 13:03:34 +0200

    definitivo para revisión

--- mainPrompt.ts
+++ mainPrompt.ts
@@ -6,17 +6,15 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-
-
 rl.question("Titulo: ", function(title) {
-    rl.question("Año de lanzamiento ", function(releaseYear: number) {
+    rl.question("Año de lanzamiento ", function(releaseYear) {
         rl.question("Actores: ", function(actors) {
             rl.question("Nacionalidad: ", function(nationality) {
                 rl.question("Director: ", function(director) {
                     rl.question("Escritor: ", function(writer) {
                         rl.question("Idioma: ", function(language) {
                             rl.question("Plataforma: ", function(platform) {
-                                rl.question("Es del Universo Marvel: ", function(isMCU: boolean) {
+                                rl.question("Es del Universo Marvel: ", function(isMCU) {
                                     rl.question("Personaje principal: ", function(mainCharacterName) {
                                         rl.question("Productor: ", function(producer) {
                                             rl.question("Distribuidor: ", function(distributor) {
@@ -30,16 +28,15 @@ rl.question("Titulo: ", function(title) {
                                                     pelicula5.setIsMCU(isMCU);
                                                     pelicula5.setMainCharacterName(mainCharacterName);
                                                     pelicula5.setProducer(producer);
-                                                    pelicula5.setDistributor(distributor);
-                                                    console.log(pelicula5);
+                                                    pelicula5.setDistributor(distributor);                                                    
                                                     rl.close();
                                                     const fs = require("fs");
                                                     let pelicula5JSON = JSON.stringify(pelicula5);
-                                                    fs.writeFileSync("pelicula5.json", pelicula5JSON);
-                                                    let imdbBBDD = fs.readFileSync("./imdbBBDD.json"); 
-                                                    let imdbJSON = JSON.parse(imdbBBDD);
-                                                    imdbJSON.push(pelicula5);
-                                                    fs.writeFileSync("newJSON.json", imdbJSON);
+                                                    let imdbBBDD2 = fs.readFileSync("./imdbBBDD.json");
+                                                    let pelicula5js = JSON.parse(pelicula5JSON);
+                                                    let imdbBBDD2js = JSON.parse(imdbBBDD2);
+                                                    imdbBBDD2js.movies.push(pelicula5js);                                        
+                                                    fs.writeFileSync("newJSON.json", imdbBBDD2js);
                                                });
                                             });
                                         });
